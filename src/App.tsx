7c817db9f466ab56a2b09088fd9d25d4cd889e09
Anthony Premo <ajaipremo@gmail.com>
2024-08-08 14:55:45 -0400

    Basic Home Page Created

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,20 @@ import Home from "./components/pages/Home";
 import Resume from "./components/pages/Resume";
 import Contact from "./components/pages/Contact";
 import Projects from "./components/pages/Projects";
+import Footer from "./components/shared/Footer.tsx";
 
 function App() {
     return (
         <Router>
             <Stack h="100vh">
-                <Navbar />
+                <Navbar/>
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/resume" element={<Resume />} />
                     <Route path="/projects" element={<Projects />} />
                     <Route path="/contact" element={<Contact />} />
                 </Routes>
+                <Footer/>
             </Stack>
         </Router>
     );
--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import {Box, Text} from "@chakra-ui/react";
 
 const Home: React.FC = () => {
     return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
+        <Box pt={20} display="flex" flexDirection="column" alignItems="center"
+             textAlign="center" px={4}
+        >
+            <Text fontSize="xxx-large" pl="10px" pt="10px" as="b">
+                Welcome to my personal website!
+            </Text>
+            <Text pl="10px">
+                My name is Anthony Premo and this page was created to showcase
+                my skills, projects, and experiences. You can navigate around
+                the website by using the navigation bar at the top of the page.
+            </Text>
+        </Box>
     );
 };
 
